bcbccebd3bbf399a0882d0312d6ecdc24751e9ab
Shayla Choque <89035883+ShaylaChoque@users.noreply.github.com>
2023-06-10 15:30:44 -0500

    Initial files

---
+++ src/test/java/com/upc/Hato/HatoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.upc.Hato;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HatoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
